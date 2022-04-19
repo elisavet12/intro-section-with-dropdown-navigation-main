99dcbeb32260f6f4653256699fcaa26fd2df0e45
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-04-19 23:00:30 +0300

    ...in progress...

--- intro.js
+++ intro.js
@@ -14,3 +14,41 @@ window.onclick = function (event) {
     }
   }
 };
+
+// const dropdownToggleFeatures = document.querySelector(".menu-btn__features");
+// const dropdownToggleCompany = document.querySelector(".menu-btn__company");
+// const dropdownFeatures = document.querySelector(".menu-content__features");
+// const dropdownCompany = document.querySelector(".menu-content__company");
+
+// dropdownToggleFeatures.addEventListener("click", () => {
+//   dropdownFeatures.classList.toggle("dropdown-active");
+// });
+
+function myFunction() {
+  var element = document.getElementById("dropdownIdFeatures");
+  element.classList.toggle("showDropdown");
+}
+
+// function myFunction() {
+//   document.getElementById("dropdownId").onclick = function () {
+//     element.classList.toggle("showDropdown");
+//   };
+// }
+
+function dropdownMenu() {
+  var element = document.getElementById("dropdownIdCompany");
+  element.classList.toggle("showDropdown");
+}
+
+// function changeImage() {
+//   if (
+//     document.getElementById("imgClickAndChange").src ==
+//     "http://www.userinterfaceicons.com/80x80/minimize.png"
+//   ) {
+//     document.getElementById("imgClickAndChange").src =
+//       "http://www.userinterfaceicons.com/80x80/maximize.png";
+//   } else {
+//     document.getElementById("imgClickAndChange").src =
+//       "http://www.userinterfaceicons.com/80x80/minimize.png";
+//   }
+// }
