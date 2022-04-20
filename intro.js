f3748ec1a67d69461ac154e7b4964367dcfb635d
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-04-20 22:20:17 +0300

    ..still in progress...

--- intro.js
+++ intro.js
@@ -52,3 +52,13 @@ function dropdownMenu() {
 //       "http://www.userinterfaceicons.com/80x80/minimize.png";
 //   }
 // }
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
