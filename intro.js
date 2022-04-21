18c8e84d904f72b13dd651b143eb82cd405ad247
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-04-21 17:55:43 +0300

    ....

--- intro.js
+++ intro.js
@@ -40,6 +40,11 @@ function dropdownMenu() {
   element.classList.toggle("showDropdown");
 }
 
+function myFunctionMobile() {
+  var element = document.getElementById("dropdownIdFeaturesMobile");
+  element.classList.toggle("showDropdown");
+}
+
 // function changeImage() {
 //   if (
 //     document.getElementById("imgClickAndChange").src ==
@@ -53,12 +58,24 @@ function dropdownMenu() {
 //   }
 // }
 
+// function openNav() {
+//   document.getElementById("mySidebar").style.width = "250px";
+//   document.getElementById("main-test").style.marginLeft = "250px";
+// }
+
+// function closeNav() {
+//   document.getElementById("mySidebar").style.width = "0";
+//   document.getElementById("main-test").style.marginLeft = "0";
+// }
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("main-mobile").style.marginLeft = "250px";
 }
 
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main-mobile").style.marginLeft = "0";
 }
