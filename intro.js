993c4068e7f5b2f3d2f42777f2fa8b8bde55de7c
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-04-24 00:37:24 +0300

    almost there!

--- intro.js
+++ intro.js
@@ -45,6 +45,11 @@ function myFunctionMobile() {
   element.classList.toggle("showDropdown");
 }
 
+function dropdownMenuMobile() {
+  var element = document.getElementById("dropdownIdCompanyMobile");
+  element.classList.toggle("showDropdown");
+}
+
 // function changeImage() {
 //   if (
 //     document.getElementById("imgClickAndChange").src ==
@@ -71,11 +76,35 @@ function myFunctionMobile() {
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("mySidebar").style.opacity = "1";
   document.getElementById("main-mobile").style.marginLeft = "250px";
+  document.getElementById("containerMobile").style.backgroundColor =
+    "hsl(0, 0%, 8%)";
+  document.getElementById("containerMobile").style.opacity = "0.75";
+
+  // document.getElementById("container").style.opacity = "0.5";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main-mobile").style.marginLeft = "0";
+  document.getElementById("containerMobile").style.backgroundColor =
+    "hsl(0, 0%, 8%)";
+  // document.getElementById("containerMobile").style.opacity = "1";
+}
+
+var dropdown = document.getElementsByClassName("menu-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
 }
